9c022581536e4ace6ad6afa2b4bcb56fd66c4480
MauricioTechtitute <MauricioTechtitute@gmail.com>
2025-12-15 07:30:04 -0500

    chore: initial CMake project (VS 2026)

---
+++ CMakeProject01/CMakeProject01.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject01.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "CMakeProject01.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
